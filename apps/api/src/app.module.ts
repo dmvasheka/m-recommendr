faa802802af3739a6ef4cedbbcd99c80a240bccd
dmitry.vasheka <vasheka.dmytro@gmail.com>
2025-12-11 12:43:15 +0200

    feat: integrate Recommendations Module into AppModule
    - Added `RecommendationsModule` to `AppModule` imports for improved modularity and functionality.

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,14 +3,17 @@ import { TmdbModule } from './tmdb/tmdb.module';
 import { EmbeddingsModule } from './embeddings/embeddings.module';
 import { MoviesModule } from './movies/movies.module';
 import { WatchlistModule } from './watchlist/watchlist.module';
+import { RecommendationsModule } from './recommendations/recommendations.module';
 
 @Module({
-    imports: [TmdbModule, EmbeddingsModule, MoviesModule,
-        WatchlistModule],
+    imports: [
+        TmdbModule,
+        EmbeddingsModule,
+        MoviesModule,
+        WatchlistModule,
+        RecommendationsModule,
+    ],
     controllers: [],
     providers: [],
 })
 export class AppModule {}
-
-
-
